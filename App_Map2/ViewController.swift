879f2c2b316cc1fc3ce41c83f59813dd5cdff9b8
CICE <cice@aula03mac01.local>
2016-04-05 19:14:10 +0200

    hola

--- App_Map2/ViewController.swift
+++ App_Map2/ViewController.swift
@@ -7,12 +7,61 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+
+enum MapType : Int {
+    case standard = 0
+    case satellite = 1
+    case hybrid = 2
+}
+
 
 class ViewController: UIViewController {
+    
+    var locationManager = CLLocationManager()
+    
+    //MARK: - IOBOUNDS
+    @IBOutlet weak var mapMyMap: MKMapView!
+    @IBOutlet weak var labelShowData: UILabel!
+    @IBOutlet weak var selectMapType: UISegmentedControl!
+    
+    //MARK: - IOACTIONS
+    @IBAction func buttonClicked(sender: AnyObject) {
+        super.viewDidLoad()
+        //crear manualmente una posicion
+        let latitude = 3.117116
+        let longitude = 101.691636
+        let latDelta = 0.005
+        let lngDelta = 0.005
+        let location : CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let span : MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lngDelta)
+        let region : MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
+        locationManager.pausesLocationUpdatesAutomatically = true
+        mapMyMap.setRegion(region, animated: true)
+    }
+    
+    
+    @IBAction func mapTypeChanged(sender: AnyObject) {
+        let mapType = MapType(rawValue: selectMapType.selectedSegmentIndex)
+        
+        switch(mapType!){
+        case.standard:
+            mapMyMap.mapType = MKMapType.Standard
+        case.satellite:
+            mapMyMap.mapType = MKMapType.Satellite
+        case.hybrid:
+            mapMyMap.mapType = MKMapType.Hybrid
+            break
+        }
+    }
 
+    //MARK: - LIFE VIEW CONTROLLER
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.desiredAccuracy = CLLocationAccuracy.
+            locationManager.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +69,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+}
 
+//MARK: - DELEGATE
+extension  ViewController : CLLocationManagerDelegate{
 }
 
